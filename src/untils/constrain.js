5d242d1afe665d0c8c5e70362261420beb07dcd6
quynhdeptrai306 <nguyentrucquynh3006@gmail.com>
2024-10-08 23:06:33 +0700

    Fix BUILD_MODE dev and production

--- src/untils/constrain.js
+++ src/untils/constrain.js
@@ -1,2 +1,11 @@
+
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE ==='prod' ) {
+  apiRoot = 'https://trello-api-alzh.onrender.com'
+}
+console.log('🚀 ~ apiRoot:', apiRoot)
 // export const API_ROOT = 'http://localhost:8017'
-export const API_ROOT = 'https://trello-api-alzh.onrender.com'
+export const API_ROOT = apiRoot
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
+  //cho phep vite dung duoc process.env
+  define: {
+    'process.env': process.env
+  },
   plugins: [
     react(),
     svgr()
