6c3487ca6b240e4c5b07caf9685fa86c232c785e
quynhdeptrai306 <nguyentrucquynh3006@gmail.com>
2024-11-18 23:42:27 +0700

    custom redux persist save user on localStorage

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,14 +1,11 @@
-// TrungQuanDev: https://youtube.com/@trungquandev
 import { useLocation } from 'react-router-dom'
 import Box from '@mui/material/Box'
 import LoginForm from './LoginForm'
 import RegisterForm from './RegisterForm'
-
 function Auth() {
   const location = useLocation()
   const isLogin = location.pathname === '/login'
   const isRegister = location.pathname === '/register'
-
   return (
     <Box sx={{
       display: 'flex',
